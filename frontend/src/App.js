10f27abe200af601a786c778c5f29af1e70f7664
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-25 21:07:05 -0700

    Import components, containers, and scss file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,18 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 }
 export default App;
