e29f0918064770a8e92393dce99f9033e933a2ef
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-25 20:25:01 -0700

    Add schema files

---
+++ backend/schemas/works.js
@@ -0,0 +1,47 @@
+export default {
+  name: "works",
+  title: "Works",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "projectLink",
+      title: "Project Link",
+      type: "string",
+    },
+    {
+      name: "codeLink",
+      title: "Code Link",
+      type: "string",
+    },
+    {
+      name: "imageurl",
+      title: "ImageURL",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [
+        {
+          name: "tag",
+          title: "Tag",
+          type: "string",
+        },
+      ],
+    },
+  ],
+};
