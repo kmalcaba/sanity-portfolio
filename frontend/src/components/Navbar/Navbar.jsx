353201d03436c7ce7f34589b2e77bbfcdbd93fcb
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-27 13:33:54 -0700

    Add logo

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,21 @@
 import "./Navbar.scss";
+import { images } from "../../constants";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="app__navbar">
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul>
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 export default Navbar;
