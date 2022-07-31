910160679f685bbb5b50ff5c60f2d243569b5331
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-30 17:25:34 -0700

    Add mobile slide out menu

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion, AnimatePresence } from "framer-motion";
 import "./Navbar.scss";
 import { images } from "../../constants";
 
+const variants = {
+  enter: {
+    x: 500,
+    opacity: 0,
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: {
+    zIndex: 0,
+    x: 500,
+    opacity: 0,
+  },
+};
+
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleHandler = () => {
+    setToggle((isOpen) => !isOpen);
+  };
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -15,6 +41,37 @@ function Navbar() {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={toggleHandler} />
+
+        <AnimatePresence>
+          {toggle && (
+            <motion.div
+              key="app__navbar-menu"
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 },
+              }}
+            >
+              <HiX onClick={toggleHandler} />
+              <ul>
+                {["home", "about", "work", "skills", "contact"].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={toggleHandler}>
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
     </nav>
   );
 }
