d3ca283627fc7daa713581071959a1ddf14858e2
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-01 17:20:07 -0700

    Get about info from Sanity

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,32 +1,43 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
+import { urlFor, client } from "../../client";
 import "./About.scss";
 
-const abouts = [
-  {
-    title: "Web development",
-    description: "I am a good web developer",
-    imageurl: images.about01,
-  },
-  {
-    title: "Web design",
-    description: "I am a good web designer",
-    imageurl: images.about02,
-  },
-  {
-    title: "UI/UX",
-    description: "I am a good web developer",
-    imageurl: images.about03,
-  },
-  {
-    title: "Web animation",
-    description: "I am a good web animator",
-    imageurl: images.about04,
-  },
-];
+// const abouts = [
+//   {
+//     title: "Web development",
+//     description: "I am a good web developer",
+//     imageurl: images.about01,
+//   },
+//   {
+//     title: "Web design",
+//     description: "I am a good web designer",
+//     imageurl: images.about02,
+//   },
+//   {
+//     title: "UI/UX",
+//     description: "I am a good web developer",
+//     imageurl: images.about03,
+//   },
+//   {
+//     title: "Web animation",
+//     description: "I am a good web animator",
+//     imageurl: images.about04,
+//   },
+// ];
 
 function About() {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
