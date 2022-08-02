033b631536f811b76090d4c695873591d4ebc2a8
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-01 19:31:44 -0700

    Add work filter functionality

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -20,7 +20,20 @@ function Work() {
     });
   }, []);
 
-  const workFilterHandler = (item) => {};
+  const workFilterHandler = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === "All") {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
 
   return (
     <>
